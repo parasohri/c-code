36cdef7237943b69333633b2593ea7afd023c189
parasohri <150348022+parasohri@users.noreply.github.com>
2023-11-09 22:47:29 +0530

    Create leaderinarray.cpp
    leaderinarray

---
+++ leaderinarray.cpp
@@ -0,0 +1,40 @@
+/*Leaders in an Array
+Problem Statement: Given an array, print all the elements which are leaders. A Leader is an element that is greater than all of the elements on its right side in the array.*/
+/*Example 1:
+Input:
+ arr = [4, 7, 1, 0]
+Output:
+ 7 1 0
+Explanation:
+ Rightmost element is always a leader. 7 and 1 are greater than the elements in their right side.
+
+Example 2:
+Input:
+ arr = [10, 22, 12, 3, 0, 6]
+Output:
+ 22 12 6
+Explanation:
+ 6 is a leader. In addition to that, 12 is greater than all the elements in its right side (3, 0, 6), also 22 is greater than 12, 3, 0, 6.*/
+#include<bits/stdc++.h>
+using namespace std;//approach 1
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+int max=0;
+for(int i=0;i<n;i++){
+    
+    for(int j=i+1;j<n;j++){
+if(arr[j]>max){
+    max=arr[j];  
+}
+}
+if(arr[i]>max){
+    cout<<arr[i]<<endl;
+}
+max =0;
+}
+}
