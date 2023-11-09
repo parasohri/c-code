3b3eda9ee30ae2d5cc0089e04efb9e0080c24457
parasohri <150348022+parasohri@users.noreply.github.com>
2023-11-09 21:34:02 +0530

    max 0 row
    max 0 row

---
+++ geeksforgeeks9nov.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxzero();
+int main(){
+int n;
+cin>>n;
+int arr[n][n];
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        cin>>arr[i][j];
+    }
+    cout<<endl;
+}
+
+int m=0;
+int s;
+    for(int i=0;i<n;i++){
+        int t=0;
+        for(int j=0;j<n;j++){
+            if(arr[i][j]==0){
+t++;
+if(t>m){
+    m=t;
+    s=i;
+}
+            }
+        }
+    }
+    cout<<"the row with max 0 is"<<s<<endl;
+
+
+
+
+}
